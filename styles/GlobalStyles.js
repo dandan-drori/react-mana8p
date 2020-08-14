17bbeacb6a9be48250d2bcb9ef13c929f8d71431
dandan-drori <dandandrorid@gmail.com>
2020-08-14 13:32:33 +0300

    1 file modified

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -1,2 +1,13 @@
-import {createGlobalStyle} from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+* {
+  padding:0;
+  margin:0;
+  box-sizing:border-box;
+}
+body {
+  margin:0;
+  padding:0;
+}
+`;
